93675dd013e3d14f6b8c75a7be9f02f5ff8df936
lokesh shriwas <lokeshshriwas2802@gmail.com>
2025-12-01 22:22:14 +0530

    fixed small related to unsubscribe logic of user

--- ws/src/User.ts
+++ ws/src/User.ts
@@ -4,40 +4,43 @@ import { SubscriptionManager } from "./SubscriptionManager";
 import { IncomingMessage, SUBSCRIBE, UNSUBSCRIBE } from "./types/in";
 
 export class User {
-    private id: string;
-    private ws: WebSocket;
+  private id: string;
+  private ws: WebSocket;
 
-    constructor(id: string, ws: WebSocket) {
-        this.id = id;
-        this.ws = ws;
-        this.addListeners();
-    }
+  constructor(id: string, ws: WebSocket) {
+    this.id = id;
+    this.ws = ws;
+    this.addListeners();
+  }
 
-    private subscriptions: string[] = [];
+  private subscriptions: string[] = [];
 
-    public subscribe(subscription: string) {
-        this.subscriptions.push(subscription);
-    }
+  public subscribe(subscription: string) {
+    this.subscriptions.push(subscription);
+  }
 
-    public unsubscribe(subscription: string) {
-        this.subscriptions = this.subscriptions.filter(s => s !== subscription);
-    }
+  public unsubscribe(subscription: string) {
+    this.subscriptions = this.subscriptions.filter((s) => s !== subscription);
+  }
 
-    emit(message: OutgoingMessage) {
-        this.ws.send(JSON.stringify(message));
-    }
+  emit(message: OutgoingMessage) {
+    this.ws.send(JSON.stringify(message));
+  }
 
-    private addListeners() {
-        this.ws.on("message", (message: string) => {
-            const parsedMessage: IncomingMessage = JSON.parse(message);
-            if (parsedMessage.method === SUBSCRIBE) {
-                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().subscribe(this.id, s));
-            }
+  private addListeners() {
+    this.ws.on("message", (message: string) => {
+      const parsedMessage: IncomingMessage = JSON.parse(message);
+      if (parsedMessage.method === SUBSCRIBE) {
+        parsedMessage.params.forEach((s) =>
+          SubscriptionManager.getInstance().subscribe(this.id, s)
+        );
+      }
 
-            if (parsedMessage.method === UNSUBSCRIBE) {
-                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().unsubscribe(this.id, parsedMessage.params[0]));
-            }
-        });
-    }
-
+      if (parsedMessage.method === UNSUBSCRIBE) {
+        parsedMessage.params.forEach((s) =>
+          SubscriptionManager.getInstance().unsubscribe(this.id, s)
+        );
+      }
+    });
+  }
 }
